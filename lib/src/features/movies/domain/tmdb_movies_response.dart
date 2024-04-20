58f753665a955ff6b5a6d27b94e8a799dfa0d851
Nosa Daniel <33612084+nosadaniel@users.noreply.github.com>
2024-04-20 19:33:42 +0200

    fix flutter analyser issue

--- lib/src/features/movies/domain/tmdb_movies_response.dart
+++ lib/src/features/movies/domain/tmdb_movies_response.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_annotation_target
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:friday_nite_movies/src/features/movies/domain/tmdb_movie.dart';
 
